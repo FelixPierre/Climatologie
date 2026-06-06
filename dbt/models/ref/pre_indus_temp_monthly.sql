de97c1770d7b3ac94dd69c04108813d8d8c29ede
Darkytzal <felix.cm.pierre@orange.fr>
2026-06-06 11:25:26 +0200

    1850-1900 reference values

---
+++ dbt/models/ref/pre_indus_temp_monthly.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+select
+    num_poste,
+    mois_mesure,
+    avg(tmin) as t_min,
+    avg(tmax) as t_max,
+    avg(tavg) as t_avg
+from {{ ref("temperature") }}
+where annee_mesure >= 1850
+    and annee_mesure <= 1900
+group by num_poste, mois_mesure
