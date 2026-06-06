520cdec5b70e34208e6bfa246c0b36bf8c5fe9ac
Darkytzal <felix.cm.pierre@orange.fr>
2026-06-06 12:24:21 +0200

    fix monthly_avg

--- dbt/models/monthly_avg.sql
+++ dbt/models/monthly_avg.sql
@@ -5,10 +5,10 @@ select
     -- nom_usuel,
     annee_mesure,
     mois_mesure,
-    avg(tmin) over w as t_min_avg,
-    avg(tmax) over w as t_max_avg,
-    avg(tmobile) over w as t_mobile_avg
+    avg(tmin) as t_min_avg,
+    avg(tmax) as t_max_avg,
+    avg(tmin_max_avg) as t_min_max_avg,
+    avg(tavg) as t_avg,
+    avg(tmobile) as t_mobile_avg
 from {{ ref("avg_temperature") }}
-window w as (
-    partition by num_poste, annee_mesure, mois_mesure
-)
+group by num_poste, annee_mesure, mois_mesure
